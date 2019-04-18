219aaa9234d60c168f4b24f6473076ffbc717cfb
Joanna Filipowska <j.filipowska@gmail.com>
2019-04-18 18:44:20 -0500

    homework

--- express/controllers/users.js
+++ express/controllers/users.js
@@ -1,33 +1,37 @@
-const { UsersModel } = require('../../mongo/models')
+const { UsersModel } = require("../../mongo/models");
 
 const defaultUser = {
-  name: 'Ted',
-  age: '24'
-}
+  name: "Ted",
+  age: "24"
+};
 
 const getAll = () => {
-  return UsersModel.find({})
-}
+  return UsersModel.find({});
+};
 
-const getOne = (name) => {
+const getOne = name => {
+  return UsersModel.findOne({});
   // find one user by name
-}
+};
 
 const createDefault = () => {
   // insert default user into db
-}
+  return UsersModel.create(defaultUser);
+};
 
-const createUser = (user) => {
+const createUser = user => {
   // insert user from POST request into db
-}
+  return UsersModel.create(user);
+};
 
 const updateUser = (name, updates) => {
   // use name as the query and updates for the updates
-}
+};
 
-const deleteUser = (name) => {
-  // use name as the query 
-}
+const deleteUser = name => {
+  // use name as the query
+  return UsersModel.delateOne({ name });
+};
 
 module.exports = {
   getAll,
@@ -36,4 +40,4 @@ module.exports = {
   createUser,
   updateUser,
   deleteUser
-}
+};
